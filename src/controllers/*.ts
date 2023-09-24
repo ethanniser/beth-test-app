54fba086904bf54edd95cf687ca240e20a3d85ac
Ethan Niser <whatplan00@gmail.com>
2023-09-24 11:50:30 -0500

    uneeded thing

--- src/controllers/*.ts
+++ src/controllers/*.ts
@@ -1,7 +1,6 @@
 import Elysia from "elysia";
 import { authController } from "./auth";
 import { chatController } from "./chat";
-import { dbController } from "./db";
 import { organization } from "./organization";
 import { ticketController } from "./ticket";
 
@@ -11,5 +10,4 @@ export const api = new Elysia({
   .use(chatController)
   .use(ticketController)
   .use(authController)
-  .use(dbController)
   .use(organization);
